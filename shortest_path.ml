c507968bb0d8c76c2c29ab13700b6880e574a9d2
watermouth <sminakuchi@gmail.com>
2013-05-09 23:29:38 +0900

    deleted unused function and test

--- shortest_path.ml
+++ shortest_path.ml
@@ -3,27 +3,9 @@
 (* sample input data *)
 let links = [|[(1,8);(3,15)];[(2,14);(3,3)];[(4,10)];[(2,5);(4,17)]|];;
 
-(* solve *)
-
-(* list up next update values *) 
-let get_candidate_values v next_nodes =
-  List.map (function (j,w) -> w + v.(j)) next_nodes;; 
-(* test *)
-let test1 = 
-  let v = Array.create 5 0 in 
-    get_candidate_values v links.(0) = [8;15]   
-let test2 = 
-  let v = Array.create 5 10 in 
-    get_candidate_values v links.(0) = [18;25]
-let test3 =
-  let v = Array.create 5 10 in 
-    get_candidate_values v links.(3) = [15;27]
-
 (* update *)
 let update num_of_nodes v links is_not_updated path = 
   for i=0 to num_of_nodes - 1 do
-    (* let candidates = get_candidate_values v links.(i) in 
-    let min_of_candidates = List.fold_left (fun x y -> min x y) max_int candidates in *)
     let min_of_candidates = List.fold_left (fun x link -> let w = (v.(fst link) + snd link) in
         if w < x then (path.(i) <- fst link; w) else x) max_int links.(i) in
     (is_not_updated.(i) <- if min_of_candidates < v.(i) 
